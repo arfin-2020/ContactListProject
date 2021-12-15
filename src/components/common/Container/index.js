6877f55464bd8379ef6b30c116a87a909cc83475
unknown <arfinchowdhuryarif7@gmail.com>
2021-12-15 07:12:13 +0600

    done

---
+++ src/components/common/Container/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ScrollView, View } from 'react-native';
+import styles from './styles';
+
+
+
+const Container = ({children}) =>{
+    return(
+        <ScrollView>
+            <View style={styles.wapper}>{children}</View>
+        </ScrollView>
+    )
+}
+
+export default Container;
